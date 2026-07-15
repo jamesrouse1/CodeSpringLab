b07fc52a307d2e15686afebddbcb7fb23a3b9ab4
James Rouse <jimmyrs19@gmail.com>
2026-07-15 12:22:50 -0400

    Expose ATAC Bowtie2 job progress and failures

--- scripts_DoNotTouch/bowtie2/bowtie2_PE.sh
+++ scripts_DoNotTouch/bowtie2/bowtie2_PE.sh
@@ -1,15 +1,39 @@
-set -euo pipefail
+set -Eeuo pipefail
 
+prefix="${1:?ERROR: output prefix is required}"
+bowtie_log="${prefix}Log.final.out"
+current_stage="initialization"
+
+report_stage() {
+	current_stage="$1"
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] ATAC Bowtie2: ${current_stage} ($(basename "${prefix}"))"
+}
+
+report_failure() {
+	local rc=$?
+	echo "ERROR: ATAC Bowtie2 failed during ${current_stage} for $(basename "${prefix}") (exit ${rc})." >&2
+	if [[ -s "$bowtie_log" ]]; then
+		echo "Last 80 lines of ${bowtie_log}:" >&2
+		tail -n 80 "$bowtie_log" >&2 || true
+	fi
+	return "$rc"
+}
+
+trap report_failure ERR
+
+report_stage "loading modules"
 module load EBModules
 module load Bowtie2/2.4.4-GCC-10.3.0
 module load SAMtools/1.14-GCC-10.3.0
 module load BEDTools/2.30.0-GCC-10.3.0
 module load picard/2.21.6-Java-11
 
+report_stage "aligning paired-end reads"
 bowtie2  --very-sensitive -k 10 -X 1000 --dovetail --threads 8 \
-	-x $2 \
-	-1 $3 -2 $4 > ${1}_temp1.sam 2> ${1}Log.final.out
+		-x $2 \
+		-1 $3 -2 $4 > ${1}_temp1.sam 2> ${1}Log.final.out
 
+report_stage "filtering and sorting alignments"
 samtools view -h -q 30 -f 0x2 -o ${1}_temp2.sam ${1}_temp1.sam
 rm ${1}_temp1.sam
 
@@ -38,6 +62,7 @@ samtools quickcheck -v ${1}Aligned.sortedByCoord.out.bam
 #O=${1}Aligned.sortedByName_removeDup.out.bam \
 #M=${1}_markedDup_metrics.txt
 
+report_stage "removing duplicates with Picard"
 java -Xmx80g -Djava.io.tmpdir="$(dirname "${1}")" -jar "$EBROOTPICARD/picard.jar" MarkDuplicates \
 REMOVE_DUPLICATES=true \
 I=${1}Aligned.sortedByCoord.out.bam \
@@ -50,6 +75,7 @@ if [[ ! -s "${1}Aligned.sortedByCoord_removeDup.out.bam" ]]; then
 fi
 samtools quickcheck -v ${1}Aligned.sortedByCoord_removeDup.out.bam
 
+report_stage "collecting insert-size metrics"
 java -Xmx80g -Djava.io.tmpdir="$(dirname "${1}")" -jar "$EBROOTPICARD/picard.jar" CollectInsertSizeMetrics \
 I=${1}Aligned.sortedByCoord_removeDup.out.bam \
 O=${1}_insert_size_metrics.txt \
@@ -64,17 +90,20 @@ samtools index -b ${1}Aligned.sortedByCoord_removeDup.out.bam ${1}Aligned.sorted
 
 #bedtools bamtobed -i ${1}Aligned.sortedByName.out.bam > ${1}Aligned.sortedByName.out.bed
 #bedtools bamtobed -i ${1}Aligned.sortedByName_removeDup.out.bam > ${1}Aligned.sortedByName_removeDup.out.bed
+report_stage "creating BED files"
 bedtools bamtobed -i ${1}Aligned.sortedByCoord.out.bam > ${1}Aligned.sortedByCoord.out.bed
 bedtools bamtobed -i ${1}Aligned.sortedByCoord_removeDup.out.bam > ${1}Aligned.sortedByCoord_removeDup.out.bed
 
 #####################################
 
+report_stage "loading deepTools"
 module load EBModules
 module load deepTools/3.5.2-foss-2022a
 
 #--effectiveGenomeSize 2913022398 \ # Mice:2150570000; GRCh38:2913022398
 ### For male mice chrX should be ignored
 
+report_stage "creating CPM bigWig"
 rm -f "${1}Aligned.sortedByCoord_removeDup.out.bw.tmp"
 if ! bamCoverage \
   -b "${1}Aligned.sortedByCoord_removeDup.out.bam" \
@@ -96,6 +125,7 @@ if [[ ! -s "${1}Aligned.sortedByCoord_removeDup.out.bw.tmp" ]]; then
 fi
 mv "${1}Aligned.sortedByCoord_removeDup.out.bw.tmp" "${1}Aligned.sortedByCoord_removeDup.out.bw"
 
+report_stage "writing alignment summary"
 mapped_reads="$(samtools view -c -F 4 ${1}Aligned.sortedByCoord.out.bam)"
 deduplicated_reads="$(samtools view -c -F 4 ${1}Aligned.sortedByCoord_removeDup.out.bam)"
 {
@@ -107,3 +137,6 @@ deduplicated_reads="$(samtools view -c -F 4 ${1}Aligned.sortedByCoord_removeDup.
   echo -e "bigwig_normalization\tCPM"
   echo -e "bigwig\t${1}Aligned.sortedByCoord_removeDup.out.bw"
 } > ${1}_alignment_summary.txt
+
+report_stage "complete"
+trap - ERR
